acf91f0ef71c2a46ad00507ccebed398eb2450c4
Krishna <krishna@findcoder.io>
2024-03-13 13:23:25 +0530

    content updated

--- src/components/InfiniteMovingCardsDemo.tsx
+++ src/components/InfiniteMovingCardsDemo.tsx
@@ -4,22 +4,33 @@ import { InfiniteMovingCards } from "@/components/ui/InfiniteMovingCards";
 
 export function InfiniteMovingCardsDemo() {
   const [results, setResults] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   const channelId = process.env.NEXT_PUBLIC_CHANNEL_ID;
   const API_KEY = process.env.NEXT_PUBLIC_API_KEY;
+
   const fetchYtVideos = async () => {
     try {
-      const res = await fetch(
-        `https://www.googleapis.com/youtube/v3/search?key=${API_KEY}&channelId=${channelId}&part=snippet,id&order=date&maxResults=4`
-      );
-      if (!res.ok) {
-        throw new Error("Failed to fetch YouTube videos: " + res.statusText);
-      }
+      const cachedData = localStorage.getItem("ytVideos");
+      if (cachedData) {
+        setResults(JSON.parse(cachedData));
+        setLoading(false);
+      } else {
+        const res = await fetch(
+          `https://www.googleapis.com/youtube/v3/search?key=${API_KEY}&channelId=${channelId}&part=snippet,id&order=date&maxResults=4`
+        );
+        if (!res.ok) {
+          throw new Error("Failed to fetch YouTube videos: " + res.statusText);
+        }
 
-      const data = await res.json();
-      setResults(data.items);
+        const data = await res.json();
+        setResults(data.items);
+        localStorage.setItem("ytVideos", JSON.stringify(data.items));
+        setLoading(false);
+      }
     } catch (error) {
       console.error("Error fetching YouTube videos:", error);
+      setLoading(false);
     }
   };
 
@@ -27,5 +38,9 @@ export function InfiniteMovingCardsDemo() {
     fetchYtVideos();
   }, []);
 
-  return <InfiniteMovingCards items={results} direction="right" speed="fast" />;
+  return loading ? (
+    <div>Loading...</div>
+  ) : (
+    <InfiniteMovingCards items={results} direction="right" speed="slow" />
+  );
 }
--- src/components/TextEffect.tsx
+++ src/components/TextEffect.tsx
@@ -1,8 +1,7 @@
 "use client";
 import { TextGenerateEffect } from "@/components/ui/TextGenerateEffect";
 
-const words = `India's most loving cyber security conference BSides
-Ahmedabad 0x05
+const words = `Dive into the cybersecurity world with flair at BSides Ahmedabad 101
 `;
 
 export default function TextGenerateEffectDemo() {
